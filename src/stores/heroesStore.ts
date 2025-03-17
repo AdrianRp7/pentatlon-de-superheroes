33a8eca09892ce34f98a20dc35c8f8869a8bbd1c
AdrianRp7 <parc769@gmail.com>
2025-03-17 22:20:16 +0100

    feat: hacer la primera version del resto de endpoints y añadir un método genérico de errores

--- src/stores/heroesStore.ts
+++ src/stores/heroesStore.ts
@@ -22,9 +22,67 @@ export const useHeroeStore = defineStore('heroe',() => {
         return messageResult;
     }
 
-    const getHero = () => {
-        
+    const getHero = async (id: string) => {
+        let messageResult = "";
+        try {
+            const result:Hero = await heroesApi.post(`/pentathlon/heroes/${id}`);
+
+            return result;
+        } catch(error) {
+            messageResult = returnErrorMessage(error)
+        }
+
+        return messageResult;
     }
 
-    return {heroes, createHero, getHero}
+    const getListHero = async () => {
+        let messageResult = "";
+        try {
+            const result:Hero[] = await heroesApi.post(`/pentathlon/heroes/`);
+
+            return result;
+        } catch(error) {
+            messageResult = returnErrorMessage(error)
+        }
+
+        return messageResult;
+    }
+
+    const updateHero = async (hero:Hero) => {
+        let messageResult = "";
+        if(hero.id) {
+            try {
+                const result:Hero = await heroesApi.put(`/pentathlon/heroes/${hero.id}`, hero);
+    
+                messageResult = `El heroe ${result.name} ha sido modificado correctamente`
+            } catch(error) {
+                messageResult = returnErrorMessage(error)
+            }
+        } else {
+            messageResult = "Debes añadir el id del heroe para modificarlo."
+        }
+
+        return messageResult;
+    }
+
+    const deleteHero = async (id: string) => {
+        let messageResult = "";
+        try {
+            const result:{done:string} = await heroesApi.post(`/pentathlon/heroes/`);
+
+            if (result.done) {
+                messageResult = "El héroe ha sido borrado con éxito"
+            } else {
+                messageResult = "El héroe no ha podido ser borrado con éxito"
+            }
+
+        } catch(error) {
+            messageResult = returnErrorMessage(error)
+        }
+
+        return messageResult;
+    }
+
+
+    return {heroes, createHero, getHero, getListHero, updateHero, deleteHero}
 })
